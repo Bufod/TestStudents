8cc2a5a753b25e5a36c4e3706d4c698b007f4989
Bufod <nikolaybonar97@gmail.com>
2019-09-21 19:15:50 +0300

    Changed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Молодцы, вы справились с GITом!");
+        System.out.println("Код изменился!");
     }
 }
